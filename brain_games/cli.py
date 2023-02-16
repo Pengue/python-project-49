4c7eefa9a2df6a383ac22ab18449df0476cdeb6d
Vadim Khamidullin <Khamidullin.VD@bk.ru>
2023-02-16 17:07:46 +0300

    first step to brain even (def brain even in cli.py)

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -4,13 +4,38 @@ import random
 
 def welcome_user():
     name = prompt.string('May I have your name? ')
-    return print(f'Hellow, {name}!')
+    print(f'Hellow, {name}!')
+    return name
 
-def brain_even():
+def check_parity(number):
+    if number % 2 == 0:
+        return True
+    return False
+
+
+def compare_answer(parity, answer):
+    if parity == True and answer == 'yes':
+        return True
+    elif parity == False and answer == 'no':
+        return True
+    return False
+
+
+def play_strange_game():
     print('Welcome to the Brain Games!')
-    welcome_user()
+    name = welcome_user()
     print('Answer "yes" if the number is even, otherwise answer "no".')
-    first_qustion = random.randint(1,100)
-    answer = prompt.string(f'Question: {first_qustion}\nYour answer: ')
-    if first_qustion % 2 == True
+    i = 0
+    while i < 3:
+        first_qustion = random.randint(1,100)
+        answer = prompt.string(f'Question: {first_qustion}\nYour answer: ')
+        if compare_answer(check_parity(first_qustion), answer) == True:
+            print('Correct!')
+            i += 1 
+        else: 
+            i = 4
+            print(f"Let's try again, {name}!")
+    if i == 3: 
+        return print(f'Congratulations, {name}!')
+
     
