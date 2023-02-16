c7a0f45c42d1f9583302bac616b0d3c659515fa8
Vadim Khamidullin <Khamidullin.VD@bk.ru>
2023-02-16 16:32:20 +0300

    updete gitignore

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,6 +1,16 @@
 import prompt
+import random
 
 
 def welcome_user():
     name = prompt.string('May I have your name? ')
     return print(f'Hellow, {name}!')
+
+def brain_even():
+    print('Welcome to the Brain Games!')
+    welcome_user()
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    first_qustion = random.randint(1,100)
+    answer = prompt.string(f'Question: {first_qustion}\nYour answer: ')
+    if first_qustion % 2 == True
+    
