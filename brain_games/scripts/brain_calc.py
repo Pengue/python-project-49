6e0193e44904817ab775a1f33633217de34bae5e
Vadim Khamidullin <Khamidullin.VD@bk.ru>
2023-02-16 23:54:50 +0300

    ordered the location of files

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-import brain_games.brain_calc
+import brain_games.games.brain_calc
 
 
 def main():
-    brain_games.brain_calc.brain_calc()
+    brain_games.games.brain_calc.brain_calc()
 
 
 if __name__ == '__main__':
