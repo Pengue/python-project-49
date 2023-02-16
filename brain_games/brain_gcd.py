7606ff23b71e6f64ade4f7399b658921d9ddc928
Vadim Khamidullin <Khamidullin.VD@bk.ru>
2023-02-16 19:23:13 +0300

    made brain-gcd

---
+++ brain_games/brain_gcd.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import math
+import prompt
+import random
+from brain_games.cli import welcome_user
+
+
+def check_answer_brain_calc(correct_answer, answer):
+    if int(correct_answer) == int(answer):
+        return True
+    else:
+        return False
+
+
+def brain_gcd():
+    print('Welcome to the Brain Games!')
+    name = welcome_user()
+    print('Find the greatest common divisor of given numbers.')
+    i = 0
+    while i < 3:
+        first_number = random.randint(1, 100)
+        second_number = random.randint(1, 100)
+        correct_answer = math.gcd(first_number, second_number)
+        answer = prompt.string(f'Question: {first_number} {second_number}\nYour answer: ')
+        if check_answer_brain_calc(correct_answer, answer) is True:
+            print('Correct!')
+            i += 1
+        else:
+            i = 4
+            print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}' .")
+            print(f"Let's try again, {name}!")
+    if i == 3:
+        return print(f'Congratulations, {name}!')
