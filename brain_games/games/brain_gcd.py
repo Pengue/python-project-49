21dd14214ae7a4176b76887642e6b4907a73cc71
Vadim Khamidullin <Khamidullin.VD@bk.ru>
2023-02-20 18:15:14 +0300

    rebuild brain-gcd

--- brain_games/games/brain_gcd.py
+++ brain_games/games/brain_gcd.py
@@ -1,27 +1,19 @@
 #!/usr/bin/env python3
 import math
-import prompt
 import random
-import brain_games.cli
-from brain_games.games.brain_calc import check_answer
+
+
+
+def rules():
+    return 'Find the greatest common divisor of given numbers.'
 
 
 def brain_gcd():
-    name = brain_games.cli.welcome_user()
-    print('Find the greatest common divisor of given numbers.')
-    i = 0
-    while i < 3:
-        first_number = random.randint(1, 100)
-        second_number = random.randint(1, 100)
-        correct_answer = math.gcd(first_number, second_number)
-        print(f'Question: {first_number} {second_number}')
-        answer = prompt.string('\nYour answer: ')
-        if check_answer(correct_answer, answer) is True:
-            print('Correct!')
-            i += 1
-        else:
-            brain_games.cli.game_over(answer, correct_answer, name)
-            break
-    if i == 3:
-        return print(f'Congratulations, {name}!')
-    return
+
+    first_number = random.randint(1, 100)
+    second_number = random.randint(1, 100)
+    correct_answer = math.gcd(first_number, second_number)
+    question = f'Question: {first_number} {second_number}'
+    return question, correct_answer
+    
+    
--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-import brain_games.games.brain_gcd
+from brain_games.game_engine import game_run
+from brain_games.games import brain_gcd
 
 
 def main():
-    brain_games.games.brain_gcd.brain_gcd()
+    game_run(brain_gcd)
 
 
 if __name__ == '__main__':
