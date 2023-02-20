2dc33ce25ac9467a65625ce1c583de4d559d518c
Vadim Khamidullin <Khamidullin.VD@bk.ru>
2023-02-20 17:54:54 +0300

    some fix

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 import brain_games.game_engine
-from brain_games.games import calc
+from brain_games.games import brain_calc
 
 
 def main():
-    brain_games.game_engine.run_game(calc)
+    brain_games.game_engine.run_game(brain_calc)
 
 
 if __name__ == '__main__':
