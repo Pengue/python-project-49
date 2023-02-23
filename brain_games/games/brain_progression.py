9417db6851927e6b0ccdfd7e9cc2dd7062cfface
Vadim Khamidullin <Khamidullin.VD@bk.ru>
2023-02-23 11:57:32 +0300

    some fix

--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -4,21 +4,22 @@ from random import randint
 RULES = 'What number is missing in the progression?'
 
 
+
 def generate_round():
     length_progression = randint(5, 10)
     start = randint(1, 10)
     step = randint(1, 10)
     secret_number = randint(0, length_progression - 1)
-    question = ' '
+    question = ''
     i = 0
-    progression = ()
     while i < length_progression:
+        if question:
+            question += ' '
         if i != secret_number:
-            progression += (start + (step * i))
-            question = question.join((progression))
+            question = question + str((start + (step * i)))
             i += 1
         else:
-            question = question.join('..')
+            question += '..'
             correct_answer = str(start + (step * i))
             i += 1
     return question, correct_answer
