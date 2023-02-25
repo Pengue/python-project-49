93a900a55fb9cf217ea90d2458ec4724f7c8fb72
Vadim Khamidullin <Khamidullin.VD@bk.ru>
2023-02-25 17:25:50 +0300

    fix some

--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -10,12 +10,12 @@ def generate_round():
     step = randint(1, 10)
     secret_number = randint(0, length_progression - 1)
     question = ' '
-    progression = [f'{start}', f'{start + step}', f'{start + step * 2}', 
-                   f'{start + step * 3}', f'{start + step * 4}', f'{start + step * 5}', 
-                   f'{start + step * 6}', f'{start + step * 7}', f'{start + step * 8}', 
+    progression = [f'{start}', f'{start + step}', f'{start + step * 2}',
+                   f'{start + step * 3}', f'{start + step * 4}',
+                   f'{start + step * 5}', f'{start + step * 6}',
+                   f'{start + step * 7}', f'{start + step * 8}',
                    f'{start + step * 9}']
     progression[secret_number] = '..'
     question = question.join(progression)
     correct_answer = str(start + (step * secret_number))
     return question, correct_answer
-generate_round()
